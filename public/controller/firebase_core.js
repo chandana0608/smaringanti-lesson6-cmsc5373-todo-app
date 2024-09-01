ee1e6a7ef448ce2233cb4d72f6d13f3995e98ce5
Srichandana Maringanti <smaringanti@uco.edu>
2024-09-01 00:45:02 -0500

    initial commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,18 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyASEHUaF6EiKZtSsFVE5CV45mIIcav4FQM",
+    authDomain: "smaringanti-5373-webapp.firebaseapp.com",
+    projectId: "smaringanti-5373-webapp",
+    storageBucket: "smaringanti-5373-webapp.appspot.com",
+    messagingSenderId: "722711439164",
+    appId: "1:722711439164:web:5bdf9a3cdb25dbdced6039"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
