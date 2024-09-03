b6bf7be4edef26cc5c0fe354adda13967baeacd9
Srichandana Maringanti <smaringanti@uco.edu>
2024-09-03 00:03:03 -0500

    init

--- public/app.js
+++ public/app.js
@@ -1,1 +1,4 @@
-console.log('from app.js')
+import { onClickHomeMenu, onClickMenu2Menu } from"./controller/menueventhandlers.js";
+//menu button handler
+document.getElementById('menu-home').onClick= onClickHomeMenu;
+document.getElementById('menu-menu2').onClick= onClickMenu2Menu;
---
+++ public/controller/menueventhandlers.js
@@ -0,0 +1,8 @@
+import { homePageView } from"../view/home_page.js_page.js";
+export function onClickHomeMenu(e) {
+    homePageView();
+}
+
+export function onClickMenu(e) {
+    menu2PageView();
+}
