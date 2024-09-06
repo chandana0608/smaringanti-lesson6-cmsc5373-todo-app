113b7d7c892843a4c4bb1e42d45d125aaefde59b
Srichandana Maringanti <smaringanti@uco.edu>
2024-09-06 09:24:29 -0500

    init

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,5 +1,19 @@
-export function signinFirebase(e) {
+import {
+    getAuth, signInWithEmailAndPassword
+ } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+ import {app} from "./firebase_core.js";
+
+
+const auth = getAuth(app);
+
+export async function signinFirebase(e) {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+
+    try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        
+    }
 }
