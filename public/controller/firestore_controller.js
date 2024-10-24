c12c50e0cf3c9d948415cc88055d02d6024e5388
Srichandana Maringanti <smaringanti@uco.edu>
2024-10-24 17:39:35 -0500

    init

--- public/controller/firestore_controller.js
+++ public/controller/firestore_controller.js
@@ -1,1 +1,18 @@
-import { } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js"
+import { 
+    getFirestore,
+    collection,
+    addDoc,
+} from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js"
+
+const TODO_TITLE_COLLECTION = 'todo_titles';
+const TODO_ITEM_COLLECTION = 'todo_items';
+
+import{app} from "./firebase_core.js";
+
+
+const db = getFirestore(app);
+
+export async function addToDoTitle(todoTitle) {
+   const docRef= await addDoc(collection(db,TODO_TITLE_COLLECTION),todoTitle.toFirestore());
+   return docRef.id;
+}
--- public/controller/home_controller.js
+++ public/controller/home_controller.js
@@ -1,4 +1,21 @@
-export function onSubmitCreateForm(e) {
+import { currentUser } from "./firebase_auth.js";
+import { addToDoTitle } from "./firestore_controller.js";
+import { ToDoTitle } from "../model/ToDoTitle.js";
+import { DEV } from "../model/constants.js";
+export async  function onSubmitCreateForm(e) {
     e.preventDefault();
-    console.log('title',e.target.title.value);
+    const title = e.target.title.value;
+    const uid = currentUser.uid;
+    const timestamp = Date.now();
+    const todoTitle = new ToDoTitle({title,uid,timestamp});
+
+    let docId;
+    try{
+        docId = await addToDoTitle(todoTitle);
+        todoTitle.set_docId(docId);
+    } catch(e) {
+        if(DEV) console.log('failed to create:',e);
+        alert('Failed to create' + JSON.stringify(e));
+        return;
+    }
 }
